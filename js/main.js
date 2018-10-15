dbe7e8fc55c44a8407a553d34ef350456e90b0b3
nipunsoni <nipun.soni@ymail.com>
2018-10-15 16:16:35 -0400

    Look and Feel Assignment files final commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-/**
- * Your JS code here
- */
-
-/**
- * Sample code
- */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
-
-  ulElement.appendChild(liElement);
-});
+//function to switch tabs
+function openTab(page, element, colour) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(page).style.display = "block";
+    element.style.backgroundColor = colour;
+}
